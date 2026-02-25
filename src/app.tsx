0d09ecf36f2b2d20463c91ab58d9a162bcc8b616
vertiljivenson9 <vertiljivenson9@gmail.com>
2026-02-25 18:33:57 -0400

    Actualizar app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
-import Navbar from './sections/navbar'
-import Hero from './sections/hero'
-import Categories from './sections/categories'
-import FeaturedFreelancers from './sections/featuredFreelancers'
-import RecentJobs from './sections/recentJobs'
-import HowItWorks from './sections/howItWorks'
-import Testimonials from './sections/testimonials'
-import CTA from './sections/cta'
-import Footer from './sections/footer'
+import { Navbar } from './sections/navbar'
+import { Hero } from './sections/hero'
+import { Categories } from './sections/categories'
+import { FeaturedFreelancers } from './sections/featuredFreelancers'
+import { RecentJobs } from './sections/recentJobs'
+import { HowItWorks } from './sections/howItWorks'
+import { Testimonials } from './sections/testimonials'
+import { CTA } from './sections/cta'
+import { Footer } from './sections/footer'
 
 function App() {
   return (
